4d68125e4ccf2a90d2049f762cc3076b72cec742
Yukwok <billynyk@gmail.com>
2022-02-22 07:55:12 +0800

    reconstruct article model

--- lib/app/data/models/article.dart
+++ lib/app/data/models/article.dart
@@ -1,38 +1,21 @@
+// import 'package:json_annotation/json_annotation.dart";
 
 import 'package:json_annotation/json_annotation.dart';
 
-
+@JsonSerializable()
 class Article {
- 
+  final String title;
+  final String author;
+
+  final String description;
+
+  final String url;
+
+  final String urlToImage;
+
+  final String content;
+
+  final String publishedAt;
 
   DateTime get getPublishAtDate => DateTime.tryParse(publishedAt);
-
 }
-
-
-{
--"source": {
-"id": "bbc-news",
-"name": "BBC News"
-},
-"author": "BBC News",
-"title": "Queen carries on with light duties after Covid symptoms",
-"description": "Her workload is likely to include the reading of state papers or looking through correspondence.",
-"url": "http://www.bbc.co.uk/news/uk-60460402",
-"urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/CBFF/production/_123332225_hi074017029.jpg",
-"publishedAt": "2022-02-21T08:52:19.748995Z",
-"content": "The Queen is working on \"light duties\" at Windsor Castle this week despite testing positive for Covid, royal officials say.\r\nBuckingham Palace confirmed on Sunday that the Queen, 95, was experiencing… [+3277 chars]"
-},
--{
--"source": {
-"id": "bbc-news",
-"name": "BBC News"
-},
-"author": "BBC News",
-"title": "Vegetables alone not enough to reduce heart risk, study finds",
-"description": "Other lifestyle factors have more of an impact on heart attacks and strokes, a large UK study suggests.",
-"url": "http://www.bbc.co.uk/news/health-60429955",
-"urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/10177/production/_123311956_gettyimages-1130791021.jpg",
-"publishedAt": "2022-02-21T08:07:23.6709135Z",
-"content": "Image source, Getty Images\r\nImage caption, Vegetables are high in fibre and nutrients, especially if eaten raw\r\nVegetables may be good for you, but eating a lot of them is unlikely to reduce your ris… [+3832 chars]"
-},
